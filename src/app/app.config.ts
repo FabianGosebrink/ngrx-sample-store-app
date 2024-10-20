33467adbc3a43dd8635f50776b0495a8fe5cb7b3
Tim Honermann <timhonermann@Tims-MacBook-Pro.local>
2024-10-20 16:03:35 +0200

    added ngrx dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
-  ],
+    provideStore(),
+    provideEffects()
+],
 };
